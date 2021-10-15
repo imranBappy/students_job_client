9747504cacf32029c7a511acbbf561f98fe35e20
imranbappy <imranhosenbappy690@gmail.com>
2021-10-15 20:20:13 +0600

    sone

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Student part time job</title>
+        <title>Student part time jobs</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
       </Head>
       <Navbar/>
